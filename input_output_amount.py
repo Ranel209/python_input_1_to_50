d56c107811735302b1d9721b4dba8a94d2f89539
Adrian Ranel <adrianranelm@gmail.com>
2024-10-30 19:29:30 +0800

    counter for range added

--- input_output_amount.py
+++ input_output_amount.py
@@ -1,4 +1,14 @@
+number_range = {
+    "0-10" : 0,
+    "11-20" : 0,
+    "21-30" : 0,
+    "31-40" : 0,
+    "41-50" : 0
+}
+
 #loop for user input
+
 #add value to variable to count
 #break loop if input error
+    
 #print input error and value of valurables
