aee1eb5276f43b760df8c4bb3e1f10d30ba2b8b4
Adrian Ranel <adrianranelm@gmail.com>
2024-10-30 23:16:40 +0800

    test counting and break

--- input_output_amount.py
+++ input_output_amount.py
@@ -12,5 +12,11 @@ while True:
     int(input("Give a number ranging from 0-50: "))
     
 #add value to variable to count
+    if user_input >= 0 and user_input <= 50:
+        if 0 <= user_input <= 10:
+            number_range["0-10"] += 1
 #break loop if input error
+    else:
+        print("error")
+    break
 #print input error and value of valurables
